fc8eb4b2b1c0d49e4823201234531890c77ab56c
sh-mug <52068539+sh-mug@users.noreply.github.com>
2023-07-01 01:44:28 +0900

    mcts

--- main.cpp
+++ main.cpp
@@ -5,10 +5,32 @@ using cell_t = int;
 using board_t = unsigned long long;
 using score_t = int;
 using score_opt = optional<score_t>;
+using reward_t = float;
 
 constexpr int Y = 7, X = 9, L = 4;
 constexpr std::array<board_t, 126> lines = { 0x0000000000204081, 0x0000000010204080, 0x0000000810204000, 0x0000040810200000, 0x0002040810000000, 0x0102040800000000, 0x0000000000408102, 0x0000000020408100, 0x0000001020408000, 0x0000081020400000, 0x0004081020000000, 0x0204081000000000, 0x000000000000000f, 0x0000000000000780, 0x000000000003c000, 0x0000000001e00000, 0x00000000f0000000, 0x0000007800000000, 0x00003c0000000000, 0x001e000000000000, 0x0f00000000000000, 0x0000000001010101, 0x0000000080808080, 0x0000004040404000, 0x0000202020200000, 0x0010101010000000, 0x0808080800000000, 0x0000000000208208, 0x0000000010410400, 0x0000000820820000, 0x0000041041000000, 0x0002082080000000, 0x0104104000000000, 0x0000000000810204, 0x0000000040810200, 0x0000002040810000, 0x0000102040800000, 0x0008102040000000, 0x0408102000000000, 0x000000000000001e, 0x0000000000000f00, 0x0000000000078000, 0x0000000003c00000, 0x00000001e0000000, 0x000000f000000000, 0x0000780000000000, 0x003c000000000000, 0x1e00000000000000, 0x0000000002020202, 0x0000000101010100, 0x0000008080808000, 0x0000404040400000, 0x0020202020000000, 0x1010101000000000, 0x0000000000410410, 0x0000000020820800, 0x0000001041040000, 0x0000082082000000, 0x0004104100000000, 0x0208208000000000, 0x0000000001020408, 0x0000000081020400, 0x0000004081020000, 0x0000204081000000, 0x0010204080000000, 0x0810204000000000, 0x000000000000003c, 0x0000000000001e00, 0x00000000000f0000, 0x0000000007800000, 0x00000003c0000000, 0x000001e000000000, 0x0000f00000000000, 0x0078000000000000, 0x3c00000000000000, 0x0000000004040404, 0x0000000202020200, 0x0000010101010000, 0x0000808080800000, 0x0040404040000000, 0x2020202000000000, 0x0000000000820820, 0x0000000041041000, 0x0000002082080000, 0x0000104104000000, 0x0008208200000000, 0x0410410000000000, 0x0000000002040810, 0x0000000102040800, 0x0000008102040000, 0x0000408102000000, 0x0020408100000000, 0x1020408000000000, 0x0000000000000078, 0x0000000000003c00, 0x00000000001e0000, 0x000000000f000000, 0x0000000780000000, 0x000003c000000000, 0x0001e00000000000, 0x00f0000000000000, 0x7800000000000000, 0x0000000008080808, 0x0000000404040400, 0x0000020202020000, 0x0001010101000000, 0x0080808080000000, 0x4040404000000000, 0x0000000001041040, 0x0000000082082000, 0x0000004104100000, 0x0000208208000000, 0x0010410400000000, 0x0820820000000000, 0x0000000004081020, 0x0000000204081000, 0x0000010204080000, 0x0000810204000000, 0x0040810200000000, 0x2040810000000000, 0x0000000008102040, 0x0000000408102000, 0x0000020408100000, 0x0001020408000000, 0x0081020400000000, 0x4081020000000000, };
 constexpr board_t end_board = 0x7FFFFFFFFFFFFFFF;
+constexpr int EXPAND_COUNT = 10;
+constexpr reward_t C_UCB = 1;
+constexpr long TL_PER_ROUND = 0.090 * CLOCKS_PER_SEC;
+
+namespace MMNMM{
+    class splitmix64{
+        unsigned long x;
+    public:
+        using result_type = unsigned long;
+        constexpr unsigned long max() const { return std::numeric_limits<unsigned long>::max(); }
+        constexpr unsigned long min() const { return std::numeric_limits<unsigned long>::min(); }
+        constexpr explicit splitmix64(unsigned long seed = 0) : x(seed) {}
+        constexpr unsigned long operator()() {
+            unsigned long z = (x += 0x9e3779b97f4a7c15);
+            z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+            z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+            return z ^ (z >> 31);
+        }
+    };
+}
+MMNMM::splitmix64 engine(std::random_device{}());
 
 struct State {
     board_t me, opp;
@@ -114,14 +136,93 @@ Result alpha_beta(State state, int depth, score_t alpha, score_t beta) {
     return memo[memo_input] = res;
 }
 
+struct MCTSnode {
+    State state;
+    reward_t reward;
+    int cnt;
+    optional<vector<MCTSnode>> children;
+    optional<int> turn;
+
+    reward_t add_reward(reward_t added_reward) {
+        assert(-2 <= added_reward && added_reward <= 2);
+        reward += (added_reward + 1) / 2;
+        ++cnt;
+        return added_reward;
+    }
+    reward_t eval() {
+        score_opt h = state.eval();
+        if (h) return add_reward(h.value());
+
+        if (!children) {
+            reward_t rollout_reward = rollout();
+            if (cnt >= EXPAND_COUNT) expand();
+            return add_reward(rollout_reward);
+        } else {
+            MCTSnode &largest = largest_ucb_child();
+            reward_t child_reward = largest.eval();
+            return add_reward(-child_reward);
+        }
+    }
+    reward_t rollout() {
+        bool is_me = true;
+        State now_state = state;
+        score_opt h = nullopt;
+        while (!(h = now_state.eval())) {
+            auto next_turns = now_state.children();
+            auto next_turn = next_turns[engine() % next_turns.size()];
+            now_state = now_state.played(next_turn);
+            is_me = !is_me;
+        }
+        return (is_me ? h.value() : -h.value());
+    }
+    void expand() {
+        children = vector<MCTSnode>{};
+        for (cell_t next_turn : state.children()) {
+            children.value().push_back(MCTSnode(state.played(next_turn), next_turn));
+        }
+    }
+    MCTSnode &largest_ucb_child() {
+        reward_t largest = -1e9;
+        size_t largest_idx = 0;
+        for (size_t i = 0; i < children.value().size(); ++i) {
+            auto &child = children.value().at(i);
+            if (child.cnt == 0) return child;
+            reward_t ucb = (-child.reward / child.cnt) + C_UCB * sqrt(log(cnt) / cnt);
+            if (largest < ucb) {
+                largest = ucb;
+                largest_idx = i;
+            }
+        }
+        return children.value().at(largest_idx);
+    }
+    void learn() {
+        clock_t deadline = TL_PER_ROUND + clock();
+        int num_sims = 0;
+        while ((++num_sims & 0xF) || clock() < deadline) {
+            eval();
+        }
+        cerr << "#sim=" << num_sims << " reward=" << reward << endl;
+    }
+    Result choose_turn() {
+        MCTSnode &largest_child = largest_ucb_child();
+        for (auto &child : children.value()) {
+            cerr << "turn=" << child.turn.value() << " count=" << child.cnt << " reward=" << child.reward << endl;
+        }
+        return Result{(score_t)largest_child.reward, largest_child.turn.value()};
+    }
+    
+    MCTSnode(State state, cell_t turn) : state(state), reward(0), cnt(0), children(nullopt), turn(turn) {}
+    MCTSnode(State state) : state(state), reward(0), cnt(0), children(nullopt), turn(nullopt) {}
+    MCTSnode() : state(State{}), reward(0), cnt(0), children(nullopt), turn(nullopt) {}
+};
+
 int main(void)
 {
+    State state;
+
     int my_id; // 0 or 1 (Player 0 plays first)
     int opp_id; // if your index is 0, this will be 1, and vice versa
     cin >> my_id >> opp_id; cin.ignore();
-
-    State state;
-    const Result INF = Result{(score_t)(int)1e9, -1};
 
     // game loop
     while (1) {
@@ -143,8 +244,9 @@ int main(void)
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
-        Result result = alpha_beta(state, 4, -INT32_MAX, INT32_MAX);
-        for (const auto cell : state.children()) cerr << cell << ','; cerr << endl;
+        MCTSnode node(state);
+        node.learn();
+        Result result = node.choose_turn();
         cerr << "score=" << result.score << " cell=" << result.cell << endl;
         state = state.play(result.value());
         cerr << state;
